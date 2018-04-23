c1f824f9bb92d299f3f32390781f445205b8c7a6
chandacharles <chandacharles904@gmail.com>
2018-04-23 15:57:49 +0200

    Made Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-import {Carousel} from './imagesCompo';
 
+import './App.css';
+import {ImagesComponent} from './imagesCompo.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Carousel/>
+      <ImagesComponent/>
       </div>
     );
   }
--- src/imagesCompo.jsx
+++ src/imagesCompo.jsx
@@ -1,77 +1,46 @@
 import React, { Component } from 'react';
-import './App.css';
-import image1 from './image.jpeg';
-import image2 from './git.jpeg';
-import image3 from './images.jpeg';
+import img1 from './git.jpeg';
+import img2 from './images.jpeg';
+import img3 from './image.jpeg';
 
 
-const imgUrls = [
-	image1,
-  image2,
-  image3
-];
+export class ImagesComponent extends Component{
+   constructor(props){
+       super(props);
+           this.imgArr = [img1,img2,img3];
+           this.state = {
+               index : 0,
+               image : this.imgArr[0]
+           }
+   }
+   next= () => {
+       let currentIndex = this.state.index;
+       let newIndex = currentIndex + 1;
+       if(newIndex === this.imgArr.length){
+           newIndex = 0;
+       }
+       this.setState({
+               index : newIndex,
+               image : this.imgArr[newIndex]
+       });
+        }
 
-export class Carousel extends React.Component {
-	constructor (props) {
-		super(props);
+      prev=()=>{
+       let currentIndex = this.state.index;
+       let newIndex = currentIndex - 1;
+       if(newIndex < 0){
+           newIndex = this.imgArr.length - 1;
+       }        this.setState({
+               index : newIndex,
+               image : this.imgArr[newIndex]
+       });
+      }
 
-		this.state = {
-			currentImageIndex: 0
-		};
-
-		this.nextSlide = this.nextSlide.bind(this);
-		this.previousSlide = this.previousSlide.bind(this);
-	}
-
-	previousSlide () {
-		const lastIndex = imgUrls.length - 1;
-		const { currentImageIndex } = this.state;
-		const shouldResetIndex = currentImageIndex === 0;
-		const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
-
-		this.setState({
-			currentImageIndex: index
-		});
-	}
-
-	nextSlide () {
-		const lastIndex = imgUrls.length - 1;
-		const { currentImageIndex } = this.state;
-		const shouldResetIndex = currentImageIndex === lastIndex;
-		const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
-
-		this.setState({
-			currentImageIndex: index
-		});
-	}
-
-	render () {
-		return (
-			<div className="carousel">
-				<Arrow direction="left" clickFunction={ this.previousSlide } glyph="&#9664;" />
-				<img src={ imgUrls[this.state.currentImageIndex] } style={{height:550 + "px",width:100 + "%"}}/>
-				<Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
-			</div>
-		);
-	}
-}
-
-const Arrow = ({ direction, clickFunction, glyph }) => (
-	<div
-		className={ `slide-arrow ${direction}` }
-		onClick={ clickFunction }>
-		{ glyph }
-	</div>
-);
-
-const ImageSlide = ({ url }) => {
-	const styles = {
-		backgroundImage: `url(${url})`,
-		backgroundSize: 'cover',
-		backgroundPosition: 'center'
-	};
-
-	return (
-		<div className="image-slide" style={styles}></div>
-	);
-}
+   render(){
+       return(<div>
+           <img src = {this.state.image} alt="img" width ="500" height ="500"/>
+           <br/>
+           <button onClick = {this.prev} type="submit">previous</button>
+           <button onClick = {this.next} type="submit">next</button>
+       </div>);
+   }}
